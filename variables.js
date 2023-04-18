b4c4d07d6c6266d75af8f52b39039e7910ddeefd
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-04-17 22:06:19 -0700

    Refactor + Calendar Prototype

---
+++ variables.js
@@ -0,0 +1,37 @@
+// Personal links
+const personalLinks = [
+    { title: 'Gmail', url: 'https://gmail.com' },
+    { title: 'Calendar', url: 'https://calendar.google.com/calendar/u/0/r/week' },
+    { title: 'Drive', url: 'https://drive.google.com/drive/u/0/my-drive' },
+];
+  
+// Professional links
+const professionalLinks = [
+    { title: 'Github', url: 'https://github.com/' },
+    { title: 'LinkedIn', url: 'https://www.linkedin.com/feed/' },
+    { title: 'Signal Techologies', url: 'https://signaltechnologies.io/' }
+];
+  
+// Streaming links
+const streamingLinks = [
+    { title: 'YouTube', url: 'https://youtube.com/' },
+    { title: 'Netflix', url: 'https://netflix.com/' },
+    { title: 'Disney+', url: 'https://www.disneyplus.com/home' },
+    { title: 'HBO Max', url: 'https://play.hbomax.com/page/urn:hbo:page:home' },
+    { title: 'Hulu', url: 'https://www.hulu.com/hub/home' }
+];
+  
+// Shopping links
+const shoppingLinks = [
+    { title: 'Amazon', url: 'https://www.amazon.com/' },
+    { title: 'Prose', url: 'https://prose.com/' },
+    { title: 'Thread Beast', url: 'https://myportal.threadbeast.com/#/app/home' },
+    { title: 'Bespoke Post', url: 'https://www.bespokepost.com/the-club-room' }
+];
+
+// Miscellaneous links
+const miscLinks = [
+    { title: 'Minecraft Server', url: 'https://aternos.org/server/' },
+    { title: 'Italy Trip', url: 'https://drive.google.com/drive/folders/1UkP56pNG16huD2mvllUthInt6RZmaL7X' },
+    { title: 'Alexa Dashboard', url: 'https://alexa.amazon.com/spa/index.html#cards' }
+];
