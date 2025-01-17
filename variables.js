5d3e0731cc9fd521e9b5d7c9b3e30b80fe2e9ca9
AmericanMaryTech <tech@americanmarywa.com>
2025-01-17 15:21:43 -0800

    American Mary Branch

--- variables.js
+++ variables.js
@@ -1,33 +1,30 @@
 // Personal links
-const personalLinks = [
+const googleLinks = [
     { title: 'Gmail', url: 'https://gmail.com' },
     { title: 'Calendar', url: 'https://calendar.google.com/calendar/u/0/r/week' },
     { title: 'Drive', url: 'https://drive.google.com/drive/u/0/my-drive' },
 ];
   
 // Professional links
-const professionalLinks = [
-    { title: 'Github', url: 'https://github.com/' },
-    { title: 'LinkedIn', url: 'https://www.linkedin.com/feed/' },
+const importantLinks = [
+    { title: 'Para Order Tracker', url: 'https://docs.google.com/spreadsheets/d/1Rr49-H9eaFQjfyKkyHNKl0G3nLl8ZwHmhz4HPVIFxmk/edit?gid=688151217#gid=688151217' },
+    { title: 'Naming Conventions', url: 'https://docs.google.com/spreadsheets/d/1CTa6Ivxu2G3Y4dh2l21dWkJBFcXqEgjXZA_n40lfcNU/edit?gid=1615341089#gid=1615341089' },
 ];
   
 // Streaming links
-const streamingLinks = [
-    { title: 'YouTube', url: 'https://youtube.com/' },
-    { title: 'Netflix', url: 'https://netflix.com/' },
-    { title: 'Disney+', url: 'https://www.disneyplus.com/home' },
-    { title: 'HBO Max', url: 'https://play.hbomax.com/page/urn:hbo:page:home' },
-    { title: 'Hulu', url: 'https://www.hulu.com/hub/home' }
+const toolsLinks = [
+    { title: 'GreenBits', url: 'https://secure.greenbits.com/' },
+    { title: 'Dutchie Admin', url: 'https://admin.dutchie.com/enterprise/11172fa7-221f-4446-b00d-f2f5499d64aa/orders/main' },
+    { title: 'Dutchie Support', url: 'https://support.dutchie.com/hc/en-us' },
 ];
   
 // Shopping links
-const shoppingLinks = [
-    { title: 'Amazon', url: 'https://www.amazon.com/' },
-    { title: 'Bespoke Post', url: 'https://www.bespokepost.com/the-club-room' }
-];
+const vendorsLinks = [
+    { title: 'GROWFLOW', url: 'https://auth.growflow.com/login?state=hKFo2SBjaVB6Z3c3aGlBTVFCVmRGUmhRU1lmeExFSzlpdDEyUKFupWxvZ2luo3RpZNkgVnluWGM1STdWUVotWHB1Z0sxUHhFQ1YtT1YwaWNzQUejY2lk2SBDMlJ3dEZLZHFVNE5wRGdBNGhJRnNuNUxIc3ZUdUVIVA&client=C2RwtFKdqU4NpDgA4hIFsn5LHsvTuEHT&protocol=oauth2&response_type=token%20id_token&redirect_uri=https%3A%2F%2Flive.growflow.com%2Fauth%2Fcomplete&scope=openid%20profile%20email%20user_metadata&audience=https%3A%2F%2Fgrowflow.com&nonce=a100de4e7ebd4a7abfe081f78c8ce6c8&auth0Client=eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4xMy40In0%3D' },
+    { title: 'Cultivera', url: 'https://wa.cultiveramarket.com/#/signin' },
+]
 
 // Miscellaneous links
-const miscLinks = [
-    { title: 'Minecraft Server', url: 'https://aternos.org/server/' },
-    { title: 'Alexa Dashboard', url: 'https://alexa.amazon.com/spa/index.html#cards' }
+const otherLinks = [
+    { title: 'Umbrella (Email & Text)', url: 'https://app.tryumbrella.com/auth/login' },
 ];
