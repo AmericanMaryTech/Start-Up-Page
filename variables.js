de2949985c30b3ed65c4bf23c351f5cfd0bcc1d6
AmericanMaryTech <tech@americanmarywa.com>
2025-01-17 17:15:21 -0800

    Update variables.js

--- variables.js
+++ variables.js
@@ -9,22 +9,29 @@ const googleLinks = [
 const importantLinks = [
     { title: 'Para Order Tracker', url: 'https://docs.google.com/spreadsheets/d/1Rr49-H9eaFQjfyKkyHNKl0G3nLl8ZwHmhz4HPVIFxmk/edit?gid=688151217#gid=688151217' },
     { title: 'Naming Conventions', url: 'https://docs.google.com/spreadsheets/d/1CTa6Ivxu2G3Y4dh2l21dWkJBFcXqEgjXZA_n40lfcNU/edit?gid=1615341089#gid=1615341089' },
+    { title: 'Receipt Tracking 2024', url: 'https://docs.google.com/spreadsheets/d/1p2pkW3i-4YIUtILVfYYrshH6EZvDT_ru81e9V00m48E/edit?gid=1343013325#gid=1343013325' },
+    { title: 'Receipt Jan Tracking 2025', url: 'https://drive.google.com/drive/folders/1TnS-PDEC1IZwIvIwfqB2RGTWWWCmv11n' },
 ];
   
 // Streaming links
 const toolsLinks = [
     { title: 'GreenBits', url: 'https://secure.greenbits.com/' },
-    { title: 'Dutchie Admin', url: 'https://admin.dutchie.com/enterprise/11172fa7-221f-4446-b00d-f2f5499d64aa/orders/main' },
+    { title: 'Dutchie Admin', url: 'https://admin.dutchie.com/' },
     { title: 'Dutchie Support', url: 'https://support.dutchie.com/hc/en-us' },
+    
 ];
   
 // Shopping links
 const vendorsLinks = [
-    { title: 'GROWFLOW', url: 'https://auth.growflow.com/login?state=hKFo2SBjaVB6Z3c3aGlBTVFCVmRGUmhRU1lmeExFSzlpdDEyUKFupWxvZ2luo3RpZNkgVnluWGM1STdWUVotWHB1Z0sxUHhFQ1YtT1YwaWNzQUejY2lk2SBDMlJ3dEZLZHFVNE5wRGdBNGhJRnNuNUxIc3ZUdUVIVA&client=C2RwtFKdqU4NpDgA4hIFsn5LHsvTuEHT&protocol=oauth2&response_type=token%20id_token&redirect_uri=https%3A%2F%2Flive.growflow.com%2Fauth%2Fcomplete&scope=openid%20profile%20email%20user_metadata&audience=https%3A%2F%2Fgrowflow.com&nonce=a100de4e7ebd4a7abfe081f78c8ce6c8&auth0Client=eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4xMy40In0%3D' },
+    { title: 'GROWFLOW', url: 'https://auth.growflow.com/login' },
     { title: 'Cultivera', url: 'https://wa.cultiveramarket.com/#/signin' },
+    { title: 'SPS Wholesale', url: 'https://spswholesale.com/' },
+    { title: 'LuvBuds', url: 'https://shopluvbuds.com/' },
 ]
 
 // Miscellaneous links
 const otherLinks = [
     { title: 'Umbrella (Email & Text)', url: 'https://app.tryumbrella.com/auth/login' },
+    { title: 'WordPress', url: 'https://my.wpengine.com/users/login' },
+    { title: 'American Mary', url: 'https://americanmarywa.com/' },
 ];
